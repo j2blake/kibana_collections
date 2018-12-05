deeb3476cb1767b8162658ed9e85da500178aa44
Jim Blake <jeb228@cornell.edu>
2018-12-05 09:43:03 -0500

    Create short versions of data files for faster testing

---
+++ scripts/wiley_files/test_wiley_files.sh
@@ -0,0 +1,8 @@
+# ------------------
+#
+# Scan through the JR1, JR5, and pricing files compiling summary stats and looking for oddities.
+#
+# ------------------
+#
+
+../../bin/wiley_files/analyze_wiley_files.rb ../../test_data/wiley_files/
